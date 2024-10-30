6fc8f0f4a95f027c8fc9620fd273ae7ecf982775
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2024-10-30 10:59:55 -0400

    Reorganize API docs (#50)
    Applies the same changes from https://github.com/Qiskit/qiskit-addon-obp/pull/24. Refer to that for more explanation.
    
    Some notes:
    
    * Removes `qiskit_addon_mpf.rst` and `qiskit_addon_mpf.slicing.transpiler.rst`, since users don't actually import these module directly 
    * Inlines all functions, but doesn't inline the transpiler pass classes because they're larger

--- qiskit_addon_utils/__init__.py
+++ qiskit_addon_utils/__init__.py
@@ -10,21 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Utilities for Qiskit addons.
-
-This package contains functionality which is meant to supplement workflows involving one or more
-Qiskit addons. For example, this package contains functions for creating Hamiltonians, generating
-Trotter time evolution circuits, and slicing and combining quantum circuits in time-wise partitions.
-
-.. currentmodule:: qiskit_addon_utils
-
-Submodules
-==========
-
-.. autosummary::
-   :toctree:
-
-   coloring
-   problem_generators
-   slicing
-"""
+# Warning: this module is not documented and it does not have an RST file.
+# If we ever publicly expose interfaces users can import from this module,
+# we should set up its RST file.
+"""Utilities for Qiskit addons."""
