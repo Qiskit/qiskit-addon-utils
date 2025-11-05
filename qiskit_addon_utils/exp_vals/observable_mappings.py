227a211120dd8aa24bd87ed9424568df029d62f0
aeddins-ibm <60495383+aeddins-ibm@users.noreply.github.com>
2025-11-05 14:50:15 -0800

    Apply suggestion from @aeddins-ibm

--- qiskit_addon_utils/exp_vals/observable_mappings.py
+++ qiskit_addon_utils/exp_vals/observable_mappings.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Utility functions for mapping between observables qubit indexing definitions."""
+"""Utility functions for mapping observables between qubit-indexing definitions."""
 
 from qiskit.quantum_info import Pauli, SparsePauliOp, SparseObservable
 
