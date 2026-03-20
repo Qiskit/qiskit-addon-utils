bb4962a8431255f86bd1b019d7c96d624c721c7b
Caleb Johnson <caleb.johnson@ibm.com>
2026-03-20 17:07:14 -0500

    Pass works but inefficient

--- qiskit_addon_utils/noise_management/post_selection/transpiler/passes/add_spectator_measures.py
+++ qiskit_addon_utils/noise_management/post_selection/transpiler/passes/add_spectator_measures.py
@@ -18,7 +18,6 @@ from __future__ import annotations
 from copy import deepcopy
 
 from qiskit.circuit import ClassicalRegister, ControlFlowOp, Qubit
-from qiskit.circuit.library import Barrier, Measure
 from qiskit.converters import circuit_to_dag
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.transpiler import CouplingMap
@@ -52,7 +51,6 @@ class AddSpectatorMeasures(TransformationPass):
         *,
         include_unmeasured: bool = True,
         spectator_creg_name: str = DEFAULT_SPECTATOR_CREG_NAME,
-        add_barrier: bool = True,
     ):
         """Initialize the pass.
 
@@ -61,8 +59,6 @@ class AddSpectatorMeasures(TransformationPass):
             include_unmeasured: Whether the qubits that are active but are not terminated by a measurement should
                 also be treated as spectators. If ``True``, a terminal measurement is added on each of them.
             spectator_creg_name: The name of the classical register added for the measurements on the spectator qubits.
-            add_barrier: Whether to add a barrier acting on all active and spectator qubits prior to the spectator
-                measurements.
         """
         super().__init__()
         self.spectator_creg_name = spectator_creg_name
@@ -73,7 +69,6 @@ class AddSpectatorMeasures(TransformationPass):
             else CouplingMap(couplinglist=coupling_map)
         )
         self.coupling_map.make_symmetric()
-        self.add_barrier = add_barrier
 
     def run(self, dag: DAGCircuit):  # noqa: D102
         active_qubits, terminated_qubits = self._find_active_and_terminated_qubits(dag)
@@ -91,20 +86,102 @@ class AddSpectatorMeasures(TransformationPass):
             unterminated_qubits = active_qubits.difference(terminated_qubits)
             spectator_qubits = spectator_qubits.union(unterminated_qubits)
 
-        if (num_spectators := len(spectator_qubits)) != 0:
-            # sort the spectator qubits, so that qubit `i` writes to clbit `i`
-            spectator_qubits_ls = list(spectator_qubits)
-            spectator_qubits_ls.sort(key=lambda qubit: qubit_map[qubit])
+        if len(spectator_qubits) == 0:
+            return dag
 
-            if self.add_barrier is True:
-                qubits = active_qubits.union(spectator_qubits_ls)
-                dag.apply_operation_back(Barrier(len(qubits)), qubits)
+        # Convert DAG to circuit for easier manipulation
+        from qiskit import QuantumCircuit
+        from qiskit.converters import dag_to_circuit
 
-            dag.add_creg(new_reg := ClassicalRegister(num_spectators, self.spectator_creg_name))
-            for qubit, clbit in zip(spectator_qubits_ls, new_reg):
-                dag.apply_operation_back(Measure(), [qubit], [clbit])
+        circuit = dag_to_circuit(dag)
 
-        return dag
+        # Create spectator register and new circuit
+        spectator_qubits_ls = list(spectator_qubits)
+        spectator_qubits_ls.sort(key=lambda qubit: qubit_map[qubit])
+        spec_reg = ClassicalRegister(len(spectator_qubits_ls), self.spectator_creg_name)
+        new_circuit = QuantumCircuit(*circuit.qregs, *circuit.cregs, spec_reg)
+
+        # Map spectator qubits to their classical bits
+        spectator_clbit_map = {qubit: spec_reg[i] for i, qubit in enumerate(spectator_qubits_ls)}
+
+        # Find which spectator qubits have entangling gates with which terminal qubits
+        spectator_entangled_with = {}  # spectator -> terminal qubit it's entangled with (or None)
+        for spectator_qubit in spectator_qubits:
+            spectator_entangled_with[spectator_qubit] = None
+            # Check all instructions for multi-qubit gates involving this spectator
+            for instruction in circuit.data:
+                if len(instruction.qubits) >= 2 and spectator_qubit in instruction.qubits:
+                    # This is a multi-qubit gate involving the spectator
+                    other_qubit = (
+                        instruction.qubits[0]
+                        if instruction.qubits[1] == spectator_qubit
+                        else instruction.qubits[1]
+                    )
+                    if other_qubit in terminated_qubits:
+                        spectator_entangled_with[spectator_qubit] = other_qubit
+                        break  # Assumption: at most one entangling gate per spectator
+
+        # Find adjacent terminal qubits for each spectator (for Case B)
+        spectator_adjacent_terminals = {}
+        for spectator_qubit in spectator_qubits:
+            adjacent = [
+                dag.qubits[neighbor_idx]
+                for neighbor_idx in self.coupling_map.neighbors(qubit_map[spectator_qubit])
+                if neighbor_idx < dag.num_qubits() and dag.qubits[neighbor_idx] in terminated_qubits
+            ]
+            spectator_adjacent_terminals[spectator_qubit] = adjacent
+
+        # Track which terminals and spectators have been synced
+        synced_terminals = set()
+        synced_spectators = set()
+
+        # Rebuild circuit, syncing spectators with terminals as we encounter terminal measurements
+        for instruction in circuit.data:
+            # If this is a terminal measurement, sync it with associated spectators
+            if (
+                instruction.operation.name == "measure"
+                and instruction.qubits[0] in terminated_qubits
+            ):
+                terminal_qubit = instruction.qubits[0]
+
+                # Find spectators that should be synced with this terminal
+                spectators_for_this_terminal = []
+                for spectator_qubit in spectator_qubits:
+                    if spectator_qubit in synced_spectators:
+                        continue  # Already synced
+
+                    # Case A: Spectator has entangling gate with this terminal
+                    if spectator_entangled_with[spectator_qubit] == terminal_qubit or (
+                        spectator_entangled_with[spectator_qubit] is None
+                        and terminal_qubit in spectator_adjacent_terminals[spectator_qubit]
+                        and terminal_qubit not in synced_terminals
+                    ):
+                        spectators_for_this_terminal.append(spectator_qubit)
+
+                # If there are spectators, sync them with this terminal
+                if spectators_for_this_terminal:
+                    # For each spectator: barrier + terminal measurement (once) + spectator measurement
+                    for i, spectator_qubit in enumerate(spectators_for_this_terminal):
+                        # Add barrier between terminal and this spectator ONLY
+                        new_circuit.barrier(terminal_qubit, spectator_qubit)
+
+                        # Add terminal measurement (only on first spectator)
+                        if i == 0:
+                            new_circuit.append(instruction)
+                            synced_terminals.add(terminal_qubit)
+
+                        # Add spectator measurement
+                        new_circuit.measure(spectator_qubit, spectator_clbit_map[spectator_qubit])
+                        synced_spectators.add(spectator_qubit)
+                else:
+                    # No spectators for this terminal, just add the measurement
+                    new_circuit.append(instruction)
+                    synced_terminals.add(terminal_qubit)
+            else:
+                # Not a terminal measurement, just add the instruction
+                new_circuit.append(instruction)
+
+        return circuit_to_dag(new_circuit)
 
     def _find_active_and_terminated_qubits(self, dag: DAGCircuit) -> tuple[set[Qubit], set[Qubit]]:
         """Helper function to find the sets of active qubits and of qubits terminated with measurements.
